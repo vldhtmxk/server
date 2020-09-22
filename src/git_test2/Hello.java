3b990eb2df6665d3cb0de9a296c167363f6dda68
Seungbeom Lee <vldhtmxk1@gmail.com>
2020-09-22 20:37:53 +0900

    화면 출력 방법 수정

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -1,18 +1,11 @@
 package git_test2;
 
 public class Hello {
+    
+    String message = "æ»≥Á«œººø‰";
 
-    String message;
-
-    public Hello() {
-        this("æ»≥Á«œººø‰");
+    public String say() {
+        return message;
     }
 
-    public Hello(String message) {
-        this.message = message;
-    }
-
-    public void say() {
-        System.out.println(message);
-    }
 }
