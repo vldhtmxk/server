7df8aa5a2bd35840c75bf5ed7225a86d63d399dd
Seungbeom Lee <vldhtmxk1@gmail.com>
2020-09-22 20:22:07 +0900

    Main.java 수정

--- src/git_test2/Main.java
+++ src/git_test2/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Hello h = new Hello();
-		h.say();
+		Hello hello = new Hello();
+		hello.say();
 
 	}
 
