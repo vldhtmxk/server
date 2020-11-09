322c5a2fdc0d0ed03afbaf759f0a21f6fc0181bf
Lee, Seungbeom <vldhtmxk1@gmail.com>
2020-11-09 22:56:13 +0900

    내가 한다

--- src/git_test2/Main.java
+++ src/git_test2/Main.java
@@ -3,8 +3,9 @@ package git_test2;
 public class Main {
 
     public static void main(String[] args) {
-        Hello hello = new Hello("hello world");
-        System.out.println(hello.say());
+        Hello h = new Hello();
+        h.say();
     }
+
 
 }
